743ea0f6f6e4a1296a4934bf1ffc66b1c75c1c96
Janet Lee <janetjhu96@gmail.com>
2018-01-30 14:24:25 -0800

    Add additional files, update .gitignore to skip the bundle.js file

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import {} from 'dotenv/config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(helmet());
+app.use(express.static(__dirname + './../client/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.post('', async (req, res) => {
+   if (!req) {
+    res.status(500).send('Bad request');
+  } else {
+    // twitter.searchTweets(req.body);
+    // const tweetArray = await twitter.searchTweets(req.body);
+    // res.json(tweetArray);
+    res.status(201);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
