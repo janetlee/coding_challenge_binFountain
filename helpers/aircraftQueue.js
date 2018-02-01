0c58c3f6689dbbe5e2013b1b4e491e243cb11345
Janet Lee <janetjhu96@gmail.com>
2018-01-31 21:09:25 -0800

    Add .babelrc and fix the imports on src/index.js

--- helpers/aircraftQueue.js
+++ helpers/aircraftQueue.js
@@ -1,46 +1,53 @@
 class Queue {
- constructor() {
-    this.aircraftQueue = [];
-    this.requestBuffer = [];
- }
-  enqueue (aircraft) {
-    this.aircraftQueue.push(aircraft);
+  constructor() {
+    this.first = [];
+    this.second = [];
+    this.third = [];
+    this.fourth = [];
   }
+
+  enqueue(aircraft) {
+    if (aircraft.type === 'P' && aircraft.size === 'L') {
+      // place in highest priority bucket
+      this.first.push(aircraft);
+    }
+
+    if (aircraft.type === 'P' && aircraft.size === 'S') {
+      // place in 2nd highest priority bucket
+      this.second.push(aircraft);
+    }
+
+    if (aircraft.type === 'C' && aircraft.size === 'L') {
+      // place in 3rd highest priority bucket
+      this.third.push(aircraft);
+    }
+
+    if (aircraft.type === 'C' && aircraft.size === 'S') {
+      // place in 4th highest priority bucket
+      this.fourth.push(aircraft);
+    }
+  }
+
 
   dequeue() {
-    let currentAircraft = this.checkAircraft(this.aircraftQueue, 'P','L');
-
-    if(currentAircraft) {
-      return currentAircraft;
-    } else {
-      currentAircraft = this.checkAircraft(this.aircraftQueue, 'P','S');
-      if (currentAircraft) {
-        return currentAircraft;
-      } else {
-        currentAircraft = this.checkAircraft(this.aircraftQueue, 'C','L');
-        if (currentAircraft) {
-          return currentAircraft;
-        } else {
-          return this.aircraftQueue.shift();
-        }
-      }
+    if(this.first.length > 0) {
+      return this.first.shift();
+    } else if(this.second.length > 0) {
+      return this.second.shift();
+    } else if(this.third.length > 0) {
+      return this.third.shift();
+    } else if(this.fourth.length > 0) {
+      return this.fourth.shift();
     }
-  }
-
-  checkAircraft(arr, type, size) {
-    var currentAircraft;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i].type === type && arr[i].size === size) {
-        currentAircraft = arr[i];
-        arr.splice(i,1);
-        break;
-      }
-    }
-
-    return currentAircraft;
-  }
-
-  addToBuffer(aircraft) {
-    this.requestBuffer.push(aircraft);
   }
 }
+
+let queueStart;
+
+const aqmRequestProcess = function(request) {
+  if(!queueStart) {
+    queueStart = new Queue();
+  }
+};
+
+export { queueStart, aqmRequestProcess }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import {} from 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import { queueStart, aqmRequestProcess } from '../helpers/aircraftQueue';
+import aircraft from '../helpers/aircraftQueue';
 
 const app = express();
 
@@ -10,13 +11,28 @@ app.use(express.static(__dirname + './../client/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
-app.post('', async (req, res) => {
+app.get('/start', (req, res) => {
+  console.log('INITIATE QUEUE');
+  aqmRequestProcess();
+  res.send('Initiated queue');
+});
+
+app.post('/enqueue', (req, res) => {
+  console.log(req);
+  if (!req) {
+    res.status(500).send('Bad request');
+  } else {
+    console.log(req);
+    Queue.enqueue(aircraft);
+    res.status(201);
+  }
+});
+
+app.get('/dequeue', (req, res) => {
    if (!req) {
     res.status(500).send('Bad request');
   } else {
-    // twitter.searchTweets(req.body);
-    // const tweetArray = await twitter.searchTweets(req.body);
-    // res.json(tweetArray);
+    helper.dequeue();
     res.status(201);
   }
 });
