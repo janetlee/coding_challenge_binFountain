2fec6a618c44f3839211b8601e7e4b212cc91800
Janet Lee <janetjhu96@gmail.com>
2018-02-02 16:54:55 -0800

    Add --exit to npm test command to close out after script finishes

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -3,6 +3,7 @@ import { expect, assert, should } from 'chai';
 import axios from 'axios';
 import src from '../src/index.js';
 
+// Possible helper function to code:
 // const postAircraft = (body, done) => {
 //     axios.post('http://127.0.0.1:3000/enqueue', body)
 //       .then(res => { response = res; })
@@ -77,7 +78,7 @@ describe('Server POST/enqueue basic tests', function() {
   it('response data should be an object', () => {
     expect(response.data).to.be.a('object');
   });
-  it('response data should contain 1 aircraft', () => {
+  it('response data should contain 1 valid PL aircraft', () => {
     expect(response.data).to.be.an('object');
     expect(response.data.first.length).to.equal(1);
     expect(response.data.second.length).to.equal(0);
@@ -86,8 +87,81 @@ describe('Server POST/enqueue basic tests', function() {
   });
 });
 
+describe('Server POST/enqueue bad aircraftType test', function() {
+  let response;
+  let body = {'aircraftType': 'X', 'aircraftSize': 'L', 'aircraftId': '12ASD1'};
 
-describe('Server POST/enqueue 2 aircraft test', function() {
+  before((done) => {
+    axios.post('http://127.0.0.1:3000/enqueue', body)
+      .then(res => { response = res; })
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  it('response data should not contain aircraftType X', () => {
+    expect(response.data).to.be.an('object');
+    expect(response.data.first.length).to.equal(1);
+    expect(response.data.second.length).to.equal(0);
+    expect(response.data.third.length).to.equal(0);
+    expect(response.data.fourth.length).to.equal(0);
+  });
+
+  body = {'aircraftType': '1', 'aircraftSize': 'L', 'aircraftId': '12ASD1'};
+
+  before((done) => {
+    axios.post('http://127.0.0.1:3000/enqueue', body)
+      .then(res => { response = res; })
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  it('response data should not contain aircraftType 1', () => {
+    expect(response.data).to.be.an('object');
+    expect(response.data.first.length).to.equal(1);
+    expect(response.data.second.length).to.equal(0);
+    expect(response.data.third.length).to.equal(0);
+    expect(response.data.fourth.length).to.equal(0);
+  });
+});
+
+describe('Server POST/enqueue bad aircraftSize test', function() {
+  let response;
+  let body = {'aircraftType': 'P', 'aircraftSize': 'M', 'aircraftId': '12ASD1'};
+
+  before((done) => {
+    axios.post('http://127.0.0.1:3000/enqueue', body)
+      .then(res => { response = res; })
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  it('response data should not contain aircraftSize M', () => {
+    expect(response.data).to.be.an('object');
+    expect(response.data.first.length).to.equal(1);
+    expect(response.data.second.length).to.equal(0);
+    expect(response.data.third.length).to.equal(0);
+    expect(response.data.fourth.length).to.equal(0);
+  });
+
+  body = {'aircraftType': 'P', 'aircraftSize': '2', 'aircraftId': '12ASD1'};
+
+  before((done) => {
+    axios.post('http://127.0.0.1:3000/enqueue', body)
+      .then(res => { response = res; })
+      .then(() => done())
+      .catch(err => done(err));
+  });
+
+  it('response data should not contain aircraftSize 2', () => {
+    expect(response.data).to.be.an('object');
+    expect(response.data.first.length).to.equal(1);
+    expect(response.data.second.length).to.equal(0);
+    expect(response.data.third.length).to.equal(0);
+    expect(response.data.fourth.length).to.equal(0);
+  });
+});
+
+describe('Server POST/enqueue 2 valid (PL, CL) aircraft test', function() {
   let response;
   let body = {'aircraftType': 'C', 'aircraftSize': 'L', 'aircraftId': '456DEF'};
 
@@ -118,7 +192,7 @@ describe('Server POST/enqueue 3 aircraft test', function() {
       .catch(err => done(err));
   });
 
-  it('response data should contain 3 aircraft', () => {
+  it('response data should contain 3 valid (PL, CL, CS) aircraft', () => {
     expect(response.data).to.be.an('object');
     expect(response.data.first.length).to.equal(1);
     expect(response.data.second.length).to.equal(0);
@@ -138,9 +212,30 @@ describe('Server POST/enqueue 4 aircraft test', function() {
       .catch(err => done(err));
   });
 
-  // before(postAircraft(body));
+  it('response data should contain 4 valid (PL, CL, CS, PS) aircraft', () => {
+    expect(response.data).to.be.an('object');
+    expect(response.data.first.length).to.equal(1);
+    expect(response.data.second.length).to.equal(1);
+    expect(response.data.third.length).to.equal(1);
+    expect(response.data.fourth.length).to.equal(1);
+  });
+});
 
-  it('response data should contain 4 aircraft', () => {
+
+xdescribe('Server POST/enqueue bad aircraftId test', function() {
+  let response;
+  let body = {'aircraftType': 'P', 'aircraftSize': 'S', 'aircraftId': []};
+
+  before((done) => {
+    axios.post('http://127.0.0.1:3000/enqueue', body)
+      .then(err => { response = err; })
+      .catch(err => done(err));
+  });
+
+  it('should respond to POST with 503 status', () => {
+    expect(response.status).to.equal(503);
+  });
+  it('response data should only contain 4 valid (PL, CL, CS, PS) aircraft', () => {
     expect(response.data).to.be.an('object');
     expect(response.data.first.length).to.equal(1);
     expect(response.data.second.length).to.equal(1);
